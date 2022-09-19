b3baa153bb0b59fab2b3f9cd4be94f43b92ea1ff
Susanta Goswami <susnitdgp@gmail.com>
2022-09-19 10:20:00 +0000

    Deleted

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\pradip-qna-app-bot-9efa-Web-Deploy.pubxml -p:Password=JDYdumiw1GRM3M4ucYWRSKvo20CcsF5wFt7xbJGbM5a01WZqlHNvfYglYhuT
-
